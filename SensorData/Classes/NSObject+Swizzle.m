7fa0bb7530542894b1a3a109d1aa3d803ba72eb9
hapy <gdhapy@gmail.com>
2022-11-06 23:40:11 +0800

    add new method to swizzle method

--- SensorData/Classes/NSObject+Swizzle.m
+++ SensorData/Classes/NSObject+Swizzle.m
@@ -34,4 +34,28 @@
     return YES;
 }
 
++ (BOOL)sensorsdata_swizzleMethodOriginalClass:(Class)originalClass withOriginalSel:(SEL)originalSEL withAlternalClass:(Class)alternateClass withMethod:(SEL)alternateSEL {
+   
+    //获取原始的方法
+    Method originalMethod = class_getInstanceMethod(self, originalSEL);
+    if (!originalMethod) {
+        return NO;
+    }
+    //获取将要交换的方法
+    Method alternateMethod = class_getInstanceMethod(alternateClass, alternateSEL);
+    if (!alternateMethod) {
+        return NO;
+    }
+    
+    //交换两个方法的实现
+    if (class_addMethod(self, originalSEL, method_getImplementation(alternateMethod), method_getTypeEncoding(alternateMethod))) {
+        class_replaceMethod(self, alternateSEL, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, alternateMethod);
+    }
+        
+    //返回yes，方法交换成功
+    return YES;
+}
+
 @end
--- SensorData/Classes/UITableView+SensorsData.m
+++ SensorData/Classes/UITableView+SensorsData.m
@@ -5,9 +5,10 @@
 //  Created by Apple on 10/31/22.
 //
 
-#define __scheme 3
+#define __scheme 1
 #import "UITableView+SensorsData.h"
 #import <objc/message.h>
+#import <objc/runtime.h>
 #import "NSObject+Swizzle.h"
 #import "UIView+TextContentData.h"
 
@@ -70,6 +71,20 @@ static void sensorData_tableViewDidSelectRow(id object,SEL selector,UITableView 
     // track data
 }
 
+- (void)sensorData_tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"original:didSelectRowAtIndexPath");
+//    [self sensorData_tableView:tableView didSelectRowAtIndexPath:indexPath];/
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+//    NSMutableArray *dataSource = [object valueForKey:@"dataSource"];
+    
+//    cell.elementType
+//    cell.elementContent;
+//    NSLog(@"sensor--->%@--%@:clicked--%@-%@", NSStringFromClass([cell class]), dataSource[indexPath.row], cell.elementType, cell.elementContent);
+    NSLog(@"%@-%@", cell.elementType, cell.elementContent);
+
+    [tableView sensorData_tableView:tableView didSelectRowAtIndexPath:indexPath];
+    
+}
 
 #pragma mark- 私有方法，负责给delegate对象添加一个方法并进行交换
 -(void)sensorData_swizzleDidSelectRowAtIndexPathMethodWithDelegate:(id)delegate {
@@ -94,11 +109,28 @@ static void sensorData_tableViewDidSelectRow(id object,SEL selector,UITableView 
     Method sourceMethod = class_getInstanceMethod(delegateClass, sourceSelector);
     const char *encoding = method_getTypeEncoding(sourceMethod);
     //当类中已经存在相同的方法时，则会添加方法失败。当时前面已经判断过方法是否存在。因此，此处一定会添加成功
-    if (!class_addMethod([delegate class], destinationSelector,(IMP)sensorData_tableViewDidSelectRow, encoding)) {
-        return;
-    }
+//    if (!class_addMethod([delegate class], destinationSelector,(IMP)sensorData_tableViewDidSelectRow, encoding)) {
+//        return;
+//    }
     
     //方法添加之后，进行方法交换
-    [delegateClass sensorsdata_swizzleMethod:sourceSelector withMethod:destinationSelector];
+//    [delegateClass sensorsdata_swizzleMethod:sourceSelector withMethod:destinationSelector];
+    [delegateClass sensorsdata_swizzleMethodOriginalClass:delegate withOriginalSel:sourceSelector withAlternalClass:[self class] withMethod:destinationSelector];
+    [self methodList];
+}
+
+- (void)methodList {
+    
+    unsigned int count = 0;
+    Method *methodList = class_copyMethodList(UITableView.class, &count);
+    NSMutableArray *list = [NSMutableArray array];
+    for (int i = 0; i < count; i ++) {
+        Method method = methodList[i];
+        SEL sel = method_getName(method);
+        [list addObject:NSStringFromSelector(sel)];
+    }
+    
+    free(methodList);
+    NSLog(@"~~~~~~:%@", list);
 }
 @end
